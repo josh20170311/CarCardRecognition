b02cb672549a6f3f5559c59c4bfdce2f3a6c2adb
josh <josh2013.880922@gmail.com>
2019-05-14 01:38:40 +0800

    Create GoogleAPI_test.py

---
+++ GoogleAPI_test.py
@@ -0,0 +1,21 @@
+import os
+import io
+#imaport the google cloud client library
+from google.cloud import vision
+from google.cloud.vision import types
+#instantiates a client
+client = vision.ImageAnnotatorClient()
+#the name of the image file to annotate
+file_name = os.path.join(os.path.dirname(__file__),'TR\\images\\car1.jpg')
+#loads the image into memory
+with io.open(file_name,'rb') as image_file :
+    content = image_file.read()
+
+image = types.Image(content = content)
+
+#performs label dtection on the image file
+response = client.text_detection(image = image)
+labels = response.text_annotations
+print('Labels:')
+for label in labels:
+    print(label.description)
