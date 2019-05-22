ac4b999bffbeb6e353bcd884f484e95092f8d81b
josh <josh2013.880922@gmail.com>
2019-05-22 11:41:17 +0800

    .

--- gui.py
+++ gui.py
@@ -3,7 +3,8 @@ import cv2
 import PIL.Image
 import PIL.ImageTk
 import time
-from tkinter import filedialog
+from tkinter import messagebox
+import os
 
 import GoogleAPI as G
 
@@ -18,7 +19,7 @@ class MyApp:
         # Variables
         self.fileName = tkinter.StringVar(value=time.strftime("%y-%m-%d-%H-%M-%S-snapshot", time.localtime()))
         self.result = tkinter.StringVar(value="Result")
-        self.lastselect = 0 #listbox
+        self.lastselect = 0  # listbox
 
         # init. widgets
         self.initwidgets()
@@ -37,9 +38,10 @@ class MyApp:
         self.vid = MyVideoCapture(self.video_source)
 
         self.canvas = tkinter.Canvas(self.window, width=self.vid.width, height=self.vid.height)
-        self.preview_canvas = tkinter.Canvas(self.window, width=self.vid.width, height=self.vid.height)
         self.preview = PIL.ImageTk.PhotoImage(file="TR/images/noimagefile.jpg")
+        self.preview_canvas = tkinter.Canvas(self.window, width=self.preview.width(), height=self.preview.height())
         self.preview_canvas.create_image(0, 0, image=self.preview, anchor=tkinter.NW)
+        self.messagebox = messagebox.Message()
 
         self.lb = tkinter.Label(self.window, text="Result", textvariable=self.result, font=("arial", 30))
         self.btn_snapshot = tkinter.Button(self.window, width=10, height=1, text="Snapshot", command=self.snapshot,
@@ -49,8 +51,8 @@ class MyApp:
         self.btn_quit = tkinter.Button(self.window, width=10, height=1, text="QUIT", command=self.window.quit,
                                        font=("arial", 30), bg='red', fg='white')
         self.listb = tkinter.Listbox(self.window, height=20, width=65, font=("arial", 15))
-        self.listb.insert(0, "TR/images/a.jpg")
-        self.listb.insert(1, "imkkkk")
+        self.makeimageslist()
+
         self.listb.selection_handle(command=self.selection_event())
 
         self.canvas.grid(column=0, row=0)
@@ -59,7 +61,7 @@ class MyApp:
         self.lb.grid(column=1, row=1)
         self.btn_snapshot.grid(column=1, row=2)
         self.btn_send.grid(column=1, row=3)
-        self.btn_quit.grid(column=1,row=4)
+        self.btn_quit.grid(column=1, row=4)
 
     def makemenu(self):
         self.main_menu = tkinter.Menu(self.window)
@@ -74,7 +76,7 @@ class MyApp:
         self.main_menu.add("command", label="about", command=self.about)
 
     def about(self):
-        pass
+        messagebox.showinfo(title='about', message='message', detail='detail')
 
     def snapshot(self):
         if self.fileName.get() == "":
@@ -83,7 +85,8 @@ class MyApp:
         ret, frame = self.vid.get_frame()
 
         if ret:
-            cv2.imwrite("images/" + self.fileName.get() + ".jpg", cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
+            cv2.imwrite("images/" + self.fileName.get() + ".png", cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
+        self.makeimageslist()
 
     def update(self):
         # Get a frame from the video source
@@ -91,15 +94,25 @@ class MyApp:
 
         if ret:
             self.photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(frame))
-            self.canvas.create_image(0, 0, image=self.photo, anchor=tkinter.NW)
+            self.canvas.create_image(0, 0, image=self.photo, anchor=tkinter.NW, )
 
         self.selection_event()
         self.window.after(self.delay, self.update)
 
+    def makeimageslist(self):
+        self.listb.delete(0, last=self.listb.size() - 1)
+        imlist = os.listdir('./images')
+        for x in imlist:
+            self.listb.insert(imlist.index(x), str(x))
+
     def selection_event(self):
         if self.listb.curselection() != () and self.listb.curselection() != self.lastselect:
-            print(self.listb.get(self.listb.curselection()))
+            self.preview = tkinter.PhotoImage(file='images/'+self.listb.get(self.listb.curselection()))
+            self.preview = self.preview.zoom(24)
+            self.preview = self.preview.subsample(int(self.preview.height()/250))
+            self.preview_canvas.create_image(0, 0, image=self.preview, anchor=tkinter.NW)
             self.lastselect = self.listb.curselection()
+
 
 class MyVideoCapture:
     def __init__(self, video_source=0):
